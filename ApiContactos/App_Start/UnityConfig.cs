aa9d542aedf418e468f8968ecd7f11e0b90b2521
Jperezaguila <jperezaguila@yahoo.es>
2016-02-09 13:37:45 +0100

    Funcional

---
+++ ApiContactos/App_Start/UnityConfig.cs
@@ -0,0 +1,28 @@
+using System.Data.Entity;
+using Microsoft.Practices.Unity;
+using System.Web.Http;
+using ApiContactos.Models;
+using ApiContactos.Repositorios;
+using Unity.WebApi;
+
+namespace ApiContactos
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<DbContext, MiniFaceEntities>();
+            container.RegisterType<UsuarioRepositorio>();
+            container.RegisterType<MensajeRepositorio>();
+            container.RegisterType<ContactoRepositorio>();
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+        }
+    }
+}
---
+++ ContactosModel/Model/MensajeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ContactosModel.Model
+{
+    public class MensajeModel
+    {
+        public int id { get; set; }
+        public int idOrigen { get; set; }
+        public int idDestino { get; set; }
+        public string asunto { get; set; }
+        public string contenido { get; set; }
+        public bool leido { get; set; }
+
+        public String Estado
+        {
+            get { return leido ? "Leido" : "Sin leer"; }
+        }
+        
+        public System.DateTime fecha { get; set; }
+    }
+}
